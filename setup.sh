7d5f7db55abe5346ff71c7d2ea7222522246a807
d4 r c k h <m4dd0x.xxx@gmail.com>
2020-10-13 12:11:33 +0300

    fix

---
+++ setup.sh
@@ -0,0 +1,26 @@
+echo Setting up the port to 80!
+echo 80 > ./PORT
+echo Wipping the Database!
+echo You have 10 seconds from now on to cancel this by pressing CTRL + C!
+echo 10
+sleep 1
+echo 9
+sleep 1
+echo 8
+sleep 1
+echo 7
+sleep 1
+echo 6
+sleep 1
+echo 5
+sleep 1
+echo 4
+sleep 1
+echo 3
+sleep 1
+echo 2
+sleep 1
+echo 1
+sleep 1
+echo WIPPING..........
+echo "{\"users: []\", \"computers\": []}" > db.json 
