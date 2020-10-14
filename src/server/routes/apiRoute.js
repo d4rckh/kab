35f96e89c8db0b45c71df9a4baf91102e4445e9d
d4 r c k h <m4dd0x.xxx@gmail.com>
2020-10-14 10:32:05 +0300

    made problems more visible + fixed api endpoints to they redirect to the correct page instead of sending OK or the user id / computer id

--- src/server/routes/apiRoute.js
+++ src/server/routes/apiRoute.js
@@ -13,17 +13,20 @@ Router.get("/domain", (req, res) => {
 Router.post("/updatedomaininformation", (req, res) => {
     dataStorage.setDNNBN(req.body.NetBios_Name)
     dataStorage.setDNDN(req.body.domainName)
-    res.sendStatus(200)
+    //res.sendStatus(200)
+    res.redirect("/domaininformation")
 })
 
 Router.post("/user", (req, res) => {
-    res.send(dataStorage.addUser({
+    const userid = (dataStorage.addUser({
         password: req.body.password,
         username: req.body.username,
         SPNs: req.body.SPNs,
         isDA: req.body.isDA,
         isService: req.body.isService
     }))
+    // res.send(userid)
+    res.redirect("/users")
 })
 
 Router.get("/user", (req, res) => {
@@ -60,7 +63,7 @@ Router.get("/problems", (req, res) => {
 })
 
 Router.post("/computer", (req, res) => {
-    res.send(dataStorage.addComputer({
+    const computerid = (dataStorage.addComputer({
         hostname: req.body.hostname,
         isServer:req.body.isServer,
         isDC: req.body.isDC,
@@ -71,6 +74,7 @@ Router.post("/computer", (req, res) => {
         isGC: req.body.isGC,
         isRODC: req.body.isRODC,
     }))
+    res.redirect("/computers")
 })
 Router.get("/computer", (req, res) => {
     res.json(dataStorage.getComputers().map(a => new ComputerClass(a)))
